f6da7e4aefa1bbd7ec63346be016242226eda8ce
NatySousa <nataliaspsousa@gmail.com>
2022-04-28 16:03:18 -0300

    Projeto Console App

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿//Localização da classe dentro do projeto
+
+using ConsoleApp1.Entities;
+using ConsoleApp1.Repositories;
+
+namespace ConsoleApp1
+{ 
+    //definição da classe
+    public class Program
+    {
+        //método para executar o projeto
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 01 -Csharp WebDeveloper");
+            Console.WriteLine("Exportar dados de cliente:");
+            Console.WriteLine("\n");
+
+            //criando um objeto da classe Cliente
+            var cliente = new Cliente();
+
+            try
+            {
+                //gerando o ID do cliente (chave guid)
+                cliente.Id = Guid.NewGuid();
+
+                Console.Write("Informe o nome do cliente........: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.Write("Informe o email do cliente.......: ");
+                cliente.Email = Console.ReadLine();
+
+                Console.Write("Informe a data de nascimento.: ");
+                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+                //criando um objeto da classe ClienteRepository
+
+                var clienteRepository = new ClienteRepository();
+
+                // gravando os dados do cliente
+
+                clienteRepository.Exportar(cliente);
+
+                Console.WriteLine("\nDados Gravados Com Sucesso!");
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nOcorreu um erro: ${e.Message}");
+
+                Console.WriteLine("\nDeseja tentar novamente? (S,N): ");
+                var opcao = Console.ReadLine();
+
+                if(opcao.Equals("S",StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Clear();//limpar conteúdo da tela
+                    Main(args); //recursividade
+                }
+
+
+    
+            }
+
+            
+
+            //pausar a execução do prompt
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Repositories/ClienteRepository.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Repositories
+{
+    public class ClienteRepository
+    {
+       //método(função) para receber os dados de um cliente e gravar em arquivo
+       public void Exportar(Cliente cliente )
+        {
+            var streamWriter = new StreamWriter("c:\\arquivos_C#\\clientes.txt" , true); //o true é usado para que o conteudo gravado no arquivo não seja sobrescrito e sim adicionado (qndo não usa o true por conta da sobrecarga ele grava em cima do arquivo anterior).
+
+            //escrever os dados do cliente no arquivo
+
+            //streamWriter.WriteLine("ID.......: " + cliente.Id); Pode ser escrito dessa forma
+            streamWriter.WriteLine($"ID......................: {cliente.Id}");
+            streamWriter.WriteLine($"Nome do Cliente.........: {cliente.Nome}");
+            streamWriter.WriteLine($"Email...................: {cliente.Email}");
+            streamWriter.WriteLine($"Data de nascimento......: {cliente.DataNascimento}");
+            streamWriter.WriteLine("\n");
+
+            //fechando o arquivo
+            streamWriter.Close();
+
+
+        }
+
+    }
+}
